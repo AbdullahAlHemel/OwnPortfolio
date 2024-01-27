2101517e0f2e6f83fa85ec7977e1babacb5a96b4
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-01-28 00:04:26 +0600

    tailwind

--- src/components/Edu/Education.jsx
+++ src/components/Edu/Education.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import czn from '../../assets/czn.jpg'
 
 const Education = () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  
   theme: {
     extend: {},
     fontFamily:{ body:['Josefin Sans', 'sans-serif'], body2:['Source Code Pro']}
