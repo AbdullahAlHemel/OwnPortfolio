4ab88fc77f0b64bb253f596f356beeebd8f91a77
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-02-02 14:28:33 +0600

    Education

--- src/components/Edu/Education.jsx
+++ src/components/Edu/Education.jsx
@@ -6,7 +6,7 @@ const Education = () => {
     return (
         <div>
             <h2 className='text-center mt-10  text-2xl font-bold text-rose-500'>Education</h2>
-          <div className='flex p-10'>
+          <div className='flex  p-10'>
           <div className='w-2/5 pl-16'> <img src={czn} alt="" /></div> 
            <div className='w-4/5'>
             <p className='text-xl text-center py-20 mx-5'>
