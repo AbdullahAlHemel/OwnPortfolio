4d21381491ba34e7bdd2212ceaf7bac926312912
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-02-10 10:37:17 +0600

    What i Do

--- src/Home/Home/WhatIDO/WhatIDo.jsx
+++ src/Home/Home/WhatIDO/WhatIDo.jsx
@@ -30,7 +30,8 @@ const WhatIDo = () => {
              <img src={tools4} alt="" className='w-12 h-10'/>
              </div>
         </div>       
-    );   
+    );  
+     
 };
 
 export default WhatIDo;
