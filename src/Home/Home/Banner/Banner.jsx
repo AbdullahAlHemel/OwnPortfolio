07d6a8482fb33fecb1cc902e4ec6b9ef03b127ab
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-02-23 08:56:29 +0600

    assets

--- src/Home/Home/Banner/Banner.jsx
+++ src/Home/Home/Banner/Banner.jsx
@@ -30,6 +30,9 @@ const Banner = () => {
             <img src='' alt="" />
             </div>
 
+
+
+
             <div className='p-16 pb-0'>
                 <img src={Fp} alt="" />
             </div>
