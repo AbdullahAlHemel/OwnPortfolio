45e8251fc15dd9458b934f689908b9b56e348b1f
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-02-26 06:29:57 +0600

    HomeRoute

--- src/Home/Home/Banner/Banner.jsx
+++ src/Home/Home/Banner/Banner.jsx
@@ -28,6 +28,8 @@ const Banner = () => {
             <a className='btn btn-outline btn-secondary' href={CV} download><FaFileDownload className='text-2xl'/>Download Resume</a>
 
 
+
+
             <img src='' alt="" />
             </div>
             <div className='p-16 pb-0'>
