f754f35238611b7c9eb1311a929ece2304c01fde
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-03-07 21:20:30 +0600

    Best Project

--- src/Home/Home/WhatIDO/WhatIDo.jsx
+++ src/Home/Home/WhatIDO/WhatIDo.jsx
@@ -4,77 +4,80 @@ import SkillIconReact from '../../../../public/Skill_Icon/react.png'
 import SkillIconCss from '../../../../public/Skill_Icon/css.png'
 import SkillIconHtml from '../../../../public/Skill_Icon/html.png'
 import SkillIconTailwind from '../../../../public/Skill_Icon/Tailwind.png'
-
 import SkillIconFirebase from '../../../../public/Skill_Icon/firebase.svg'
 import SkillIconNodeJs from '../../../../public/Skill_Icon/js.png'
 import SkillIconMongo from '../../../../public/Skill_Icon/mongodb.svg'
 import SkillIconReactRouter from '../../../../public/Skill_Icon/react-router.svg'
+import Github from '../../../../public/Tools/github.png'
+import Figma from '../../../../public/Tools/Figma.png'
+import Git from '../../../../public/Tools/Git.png'
+import VsCode from '../../../../public/Tools/vsCode.png'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const WhatIDo = () => {
+    AOS.init({duration:1000})
     return (
         <div>
-            <h2 className='uppercase text-center font-semibold text-2xl pt-12'>Skills</h2>
-           <h2 className='text-center mt-8 text-xl border font-bold text-[#00b5ff]'>Front-End</h2>
-   
-    <div className='mx-auto w-[700px] pt-14 pb-5 grid grid-cols-5'>
-            
-           <div className='hover:border-pink-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-orange-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconJava} alt="" /></div></div> 
-           <div className='hover:border-amber-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-cyan-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconReact} alt="" /></div></div> 
-           <div className='hover:border-red-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-sky-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconCss} alt="" /></div></div> 
-           <div className='hover:border-sky-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-orange-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconHtml} alt="" /></div></div> 
-           <div className='hover:border-pink-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-blue-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconTailwind} alt="" /></div></div> 
-     
+            <h2 className='uppercase text-center font-semibold text-2xl pt-12 font-body2 text-lime-700'>Skills</h2>
+
+         {/* Front-end */}
+           <div className='text-center mt-8 text-xl mx-[570px] pb-1.5 border-slate-400 border-b-2 rounded-md font-bold text-zinc-700 font-body3'>Front-End</div>
+
+         <div className='mx-auto w-[700px] pt-14 pb-5 grid grid-cols-5 '>
+
+           <div  style={{transition:'0.5s'}} data-aos="flip-left"  className='hover:border-pink-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 pt-8 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-blue-300 hover:shadow-xl '><div className=''><img  className='' src={SkillIconTailwind} alt="" /></div></div> 
+           <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-pink-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-orange-300 hover:shadow-xl duration-500'><div className=''><img className='' src={SkillIconJava} alt="" /></div></div> 
+           <div style={{transition:'1s'}} data-aos="flip-left" className='border-amber-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg shadow-cyan-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconReact} alt="" /></div></div> 
+           <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-red-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-sky-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconCss} alt="" /></div></div> 
+           <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-sky-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-orange-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconHtml} alt="" /></div></div> 
+
             <div className='mx-auto w-[700px] grid grid-cols-5 mt-1.5  font-medium font-body3 tracking-[.03em]'>
+                <div className='w-[70px] mx-auto font-medium'> <h3 className='text-center '>Tailwind</h3></div> 
                 <div className=' w-[70px] mx-auto font-medium'> <h3 className='text-center'>JavaScript</h3></div> 
                 <div className='w-[70px] mx-auto font-medium'><h3 className='text-center '>React</h3></div> 
                 <div className='w-[70px] mx-auto font-medium'> <h3 className='text-center '>CSS</h3></div> 
                 <div className='w-[70px] mx-auto font-medium'> <h3 className='text-center'>Html</h3></div> 
-                <div className='w-[70px] mx-auto font-medium'> <h3 className='text-center '>Tailwind</h3></div> 
             </div>
-    </div>
+
+          </div>
    
-    <div className='text-center mt-8 text-xl border font-bold text-[#ff00d3]'>Back-End</div>
+    {/* Backend    */}
+    <div className='text-center mt-8 text-xl mx-[570px] pb-1.5 border-slate-400 border-b-2 rounded-md font-bold text-zinc-700 font-body3'>Back-End</div>
 
     <div className='mx-auto w-[570px] pt-14 pb-5 grid grid-cols-4'>
             
-            <div className='hover:border-green-200 hover:bg-gradient-to-t from-pink-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-fuchsia-300 hover:shadow-xl duration-150'><div className=''><img className='w-[60px] ml-2' src={SkillIconFirebase} alt="" /></div></div> 
-            <div className='hover:border-amber-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-emerald-200 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconNodeJs} alt="" /></div></div> 
-            <div className='hover:border-red-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-sky-300 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconMongo} alt="" /></div></div> 
-            <div className='hover:border-sky-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-orange-300 hover:shadow-xl duration-150 pt-[40px]'><div className=''><img className='' src={SkillIconReactRouter} alt="" /></div></div> 
+            <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-green-200 hover:bg-gradient-to-t from-pink-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-fuchsia-300 hover:shadow-xl duration-150'><div className=''><img className='w-[50px] ml-2' src={SkillIconFirebase} alt="" /></div></div> 
+            <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-amber-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-emerald-200 hover:shadow-xl duration-150'><div className=''><img className='' src={SkillIconNodeJs} alt="" /></div></div> 
+            <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-red-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-violet-300 shadow-xl duration-150'><div className=''><img className='' src={SkillIconMongo} alt="" /></div></div> 
+            <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-sky-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-orange-300 hover:shadow-xl duration-150 pt-[30px]'><div className=''><img className='' src={SkillIconReactRouter} alt="" /></div></div> 
       
              <div className='mx-auto w-[570px] grid grid-cols-4 mt-1.5  font-medium font-body3 tracking-[.03em]'>
-                 <div className=' w-[70px] mx-auto font-medium'> <h3 className='text-center'>JavaScript</h3></div> 
-                 <div className='w-[70px] mx-auto font-medium'><h3 className='text-center '>React</h3></div> 
-                 <div className='w-[70px] mx-auto font-medium'> <h3 className='text-center '>CSS</h3></div> 
-                 <div className='w-[70px] mx-auto font-medium'> <h3 className='text-center'>Html</h3></div> 
+                 <div className='  mx-auto font-medium'> <h3 className='text-center'>Firebase</h3></div> 
+                 <div className=' mx-auto font-medium'><h3 className='text-center '>Express Js</h3></div> 
+                 <div className='mx-auto font-medium'> <h3 className='text-center '>MongoDB</h3></div> 
+                 <div className=' mx-auto font-medium'> <h3 className='text-center'>React Router</h3></div> 
              </div>
      </div>
 
+x
+     {/* Tools */}
+     <div className='text-center mt-8 text-xl mx-[570px] pb-1.5 border-slate-400 border-b-2 rounded-md font-bold text-zinc-700 font-body3'>Tools</div>
 
-
-
+    <div className='mx-auto w-[570px] pt-14 pb-5 grid grid-cols-4'>
             
-           {/* <div className='grid md:grid-cols-2 p-10 gap-7'>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>JavaScript</h2><progress className="progress progress-info  w-100 h-3 mt-2.5 ml-5" value="50" max="100"></progress></h2>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>React</h2><progress className="progress progress-info w-100 h-3 mt-2.5 ml-5" value="75" max="100"></progress></h2>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>CSS</h2><progress className="progress progress-info w-100 h-3 mt-2.5 ml-5" value="85" max="100"></progress></h2>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>Html</h2><progress className="progress progress-info w-100 h-3 mt-2.5 ml-5" value="80" max="100"></progress></h2>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>Tailwind</h2><progress className="progress progress-info w-100 h-3 mt-2.5 ml-5" value="80" max="100"></progress></h2>
-            </div> */}
-           {/* <h2 className='text-center mt-8 text-xl border font-bold text-[#ff00d3]'>Back-End</h2>
-           <div className='grid md:grid-cols-2 p-10 gap-7'>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>MongoDB</h2><progress className="progress progress-secondary  w-100 h-3 mt-2.5 ml-5" value="65" max="100"></progress></h2>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>Firebase</h2><progress className="progress progress-secondary  w-100 h-3 mt-2.5 ml-5" value="50" max="100"></progress></h2>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>React-Router</h2><progress className="progress progress-secondary w-100 h-3 mt-2.5 ml-5" value="60" max="100"></progress></h2>
-            <h2 className='flex'><h2 className='text-xl font-semibold w-[85px]'>Express</h2><progress className="progress progress-secondary w-100 h-3 mt-2.5 ml-5" value="60" max="100"></progress></h2>
-            </div> */}
-            {/* <h2 className='text-center mt-8 text-xl border font-bold text-[#ff5200]'>Tools</h2>
-             <div className='flex justify-center gap-5 my-4'>
-             <img src={tools1} alt="" className='w-10 h-10'/>
-             <img src={tools2} alt="" className='w-12 h-10'/>          
-             <img src={tools3} alt="" className='w-11 h-10'/>       
-             <img src={tools4} alt="" className='w-12 h-10'/>
-             </div>                   */}
+            <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-green-200 hover:bg-gradient-to-t from-pink-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-blue-300 hover:shadow-xl duration-150'><div className=''><img className='w-[50px] ml-2' src={Figma} alt="" /></div></div> 
+            <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-amber-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg  shadow-emerald-200 shadow-xl duration-150'><div className=''><img className='' src={Github} alt="" /></div></div> 
+            <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-red-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-sky-300 hover:shadow-xl duration-150'><div className=''><img className='' src={Git} alt="" /></div></div> 
+            <div style={{transition:'0.5s'}} data-aos="flip-left" className='hover:border-sky-200 hover:bg-gradient-to-t from-red-50 to-transparent w-[120px] px-6 py-5 mx-auto font-semibold border rounded-lg shadow-lg hover:shadow-red-300 hover:shadow-xl duration-150 pt-[30px]'><div className=''><img className='' src={VsCode} alt="" /></div></div> 
+      
+             <div className='mx-auto w-[570px] grid grid-cols-4 mt-1.5  font-medium font-body3 tracking-[.03em]'>
+                 <div className='  mx-auto font-medium'> <h3 className='text-center'>Figma</h3></div> 
+                 <div className=' mx-auto font-medium'><h3 className='text-center '>Github</h3></div> 
+                 <div className='mx-auto font-medium'> <h3 className='text-center '>Git</h3></div> 
+                 <div className=' mx-auto font-medium'> <h3 className='text-center'>VS Code</h3></div> 
+             </div>
+     </div>
         </div>  
     );    
 };
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,38 +2,80 @@ import React from 'react';
 import img1 from '../../../public/Screenshot (124).png'
 import img2 from '../../../public/Screenshot (125).png'
 import img3 from '../../../public/Screenshot (126).png'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Projects = () => {
-    return (<>
-                       <h2 className='text-center text-2xl mt-9 font-bold text-emerald-500'>Best Projects</h2>
-        <div className='my-6 max-w-6xl m-auto md:flex '>
-            <div className='w-1/3'>
-                <img src={img1} alt="" />
-                <h2 className='text-center font-bold text-2xl mt-3'>News Blog Projects</h2>
-                <div className='text-center mt-5'>
-                <a className='btn mr-8' href="https://github.com/AbdullahAlHemel/blog-client">Code Link</a>
-                <a className='btn mr-8' href="https://assignment-11-3127e.web.app/">Live Link</a>
+    AOS.init({duration:1000})
+    return (<div className='bg-slate-100 py-1 my-8 pb-10'>
+            <h2 className=' text-center text-2xl mt-11 font-semibold pb-5 text-emerald-500 font-body2'>Best Projects</h2>
+       
+       
+        <div className='my-6 max-w-4xl m-auto md:flex '>
+            <div data-aos="fade-right"  className='bg-white p-8 hover:scale-105 duration-300 flex border-y-2 border-r-2 rounded-r-xl'>
+                <div className='w-1/2 float-left'>
+                  <a className='' href="https://assignment-11-3127e.web.app/"><img className='w-10/12' src={img1} alt="" /></a>
+                </div>
+                <div className='w-1/2 float-right my-auto'>
+                    <h2 className='text-center font-semibold text-2xl mt-3 font-body3 pb-5'>News Blog Projects</h2>
+                    <div className='text-center mt-5'>
+                    <a className='w-10 h-5  p-3 mr-2 rounded-md font-body2 font-semibold border hover:shadow-xl hover:w-12  border-emerald-600' href="https://github.com/AbdullahAlHemel/blog-client">Code Link</a>
+                    <a className='w-10 h-5  p-3 mr-2 rounded-md font-body2 font-semibold border hover:shadow-xl hover:w-12  border-yellow-800' href="https://assignment-11-3127e.web.app/">Live Link</a>
+                    </div>
                 </div>
             </div>
-            <div className='w-1/3'>
+        </div>
+        {/* 2nd project */}
+        <div className='my-6 max-w-4xl m-auto md:flex '>
+            <div data-aos="fade-left"  className='bg-white p-8 hover:scale-125 duration-300 flex border-y-2 border-l-2 rounded-l-xl'>
+                
+                <div className='w-1/2 float-left my-auto'>
+                    <h2 className='text-center font-semibold text-2xl mt-3 font-body3 pb-5'>Car Brand Projects</h2>
+                    <div className='text-center mt-5'>
+                    <a className='w-10 h-5  p-3 mr-2 rounded-md font-body2 font-semibold border hover:shadow-xl hover:w-12  border-emerald-600' href="https://github.com/AbdullahAlHemel/car-World">Code Link</a>
+                    <a className='w-10 h-5  p-3 mr-2 rounded-md font-body2 font-semibold border hover:shadow-xl hover:w-12  border-yellow-800' href="https://assignment-10-ae122.web.app/">Live Link</a>
+                    </div>
+                </div>
+                <div className='w-1/2 float-right'>
+                  <a className='' href="https://assignment-10-ae122.web.app/"><img className='w-10/12' src={img2} alt="" /></a>
+                </div>
+            </div>
+        </div>
+        <div className='my-6 max-w-4xl m-auto md:flex '>
+            <div data-aos="fade-right"  className='bg-white p-8 hover:scale-105 duration-300 flex border-y-2 border-r-2 rounded-r-xl'>
+                <div className='w-1/2 float-left'>
+                  <a className='' href="https://m-53-5a63b.firebaseapp.com/"><img className='w-10/12' src={img3} alt="" /></a>
+                </div>
+                <div className='w-1/2 float-right my-auto'>
+                    <h2 className='text-center font-semibold text-2xl mt-3 font-body3 pb-5'>Carrier Course Projects</h2>
+                    <div className='text-center mt-5'>
+                    <a className='w-10 h-5  p-3 mr-2 rounded-md font-body2 font-semibold border hover:shadow-xl hover:w-12  border-emerald-600' href="https://github.com/AbdullahAlHemel/courseWeb">Code Link</a>
+                    <a className='w-10 h-5  p-3 mr-2 rounded-md font-body2 font-semibold border hover:shadow-xl hover:w-12  border-yellow-800' href="https://m-53-5a63b.firebaseapp.com/">Live Link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+            {/* project2 */}
+            {/* <div className='w-1/3'>
                 <img src={img2} alt="" />
-                <h2 className='text-center font-bold text-2xl mt-3'>Car World Projects</h2>
+                <h2 className='text-center font-bold text-2xl mt-3'></h2>
                 <div className='text-center mt-5'>
-                <a className='btn mr-8' href="https://github.com/AbdullahAlHemel/car-World">Code Link</a>
-                <a className='btn mr-8' href="https://assignment-10-ae122.web.app/">Live Link</a>
+                <a className='btn mr-8' href="">Code Link</a>
+                <a className='btn mr-8' href="">Live Link</a>
                 </div>
             </div>
             <div className='w-1/3'>
                 <img src={img3} alt="" />
-                <h2 className='text-center font-bold text-2xl mt-3'>Course Registration Projects</h2>
+                <h2 className='text-center font-bold text-2xl mt-3'>Course selling Projects</h2>
                 <div className='text-center mt-5'>
                 <a className='btn mr-8' href="https://github.com/AbdullahAlHemel/blog-client">Code Link</a>
-                <a className='btn mr-8' href="https://coursebox-rkyhck98i-abdullahalhemel.vercel.app/">Live Link</a>
+                <a className='btn mr-8' href="">Live Link</a>
                 </div>
-            </div>
+            </div> */}
+            <div>
             <div></div>
-            <div></div>
-        </div>    </>
+        </div>   
+     </div>
 
     );
 };
