b99d1b88f4c833a61c05ccc6fc640f37db24f601
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-03-09 11:51:03 +0600

    Contract

--- src/components/Edu/Education.jsx
+++ src/components/Edu/Education.jsx
@@ -13,10 +13,10 @@ const Education = () => {
            <div className='w-4/5 float-right'  data-aos="zoom-in-left">
             <p className='text-xl text-center py-8 mx-5'>
                 <h2 className='text-2xl font-body4 font-bold'>University of Dhaka</h2>
-                <h2 className='text-2xl font-body4 border-b w-60 m-auto my-1'></h2>
+                <h2 className='text-2xl font-body4 border-b border-[1px] w-60 m-auto my-1'></h2>
                 <h2 className='text-xl font-body2 '>(2023 - Present)</h2> 
-                <p className='font-body1 pt-1 w-96 m-auto font-medium text-xl'>Bachelor of social science </p>
-                <p className='font-body1 pt-0.5 w-96 m-auto font-medium text-lg'> Department of Peace and conflict studies</p>
+                <p className='font-body3 pt-1 w-96 m-auto font-sm text-xl'>Bachelor of social science </p>
+                <p className='font-body3 pt-0.5 w-96 m-auto font-sm text-lg'> Department of Peace and conflict studies</p>
                 </p></div>
            </div>
            
